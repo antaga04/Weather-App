1386f28775e69bd8438f421e571bb3a475405890
antaga04 <antaga04@gmail.com>
2024-05-09 20:12:52 +0200

    feat: clean component using template

--- src/pages/Hours/Hours.jsx
+++ src/pages/Hours/Hours.jsx
@@ -1,111 +1,12 @@
 import React from 'react';
 import './Hours.css';
-import { CloudIcon } from '../../components/Icons/Icons';
-import Card from '../../components/Card/Card';
+import WeatherComponent from '../../components/WeatherComponent';
+import HoursTemplate from './HoursTemplate';
+import { FORECAST_API_URL } from '../../services/API';
 
 const Hours = () => {
   return (
-    <main className="hours-section">
-      <h1>Bialystok, PL</h1>
-      <ul>
-        <li className="">
-          <div className="hour-info">
-            <p className="time">24</p>
-            <div className="icon">{CloudIcon()}</div>
-            <p className="description">Overcast Clouds</p>
-            <div className="temp">
-              <p>13 ºC</p>
-              <p>/Feels like</p>
-              <p>12 ºC</p>
-            </div>
-          </div>
-        </li>
-        <li className="">
-          <div className="hour-info">
-            <p className="time">03</p>
-            <div className="icon">{CloudIcon()}</div>
-            <p className="description">Overcast Clouds</p>
-            <div className="temp">
-              <p>13 ºC</p>
-              <p>/Feels like</p>
-              <p>12 ºC</p>
-            </div>
-          </div>
-        </li>
-        <li className="">
-          <div className="hour-info">
-            <p className="time">07</p>
-            <div className="icon">{CloudIcon()}</div>
-            <p className="description">Overcast Clouds</p>
-            <div className="temp">
-              <p>13 ºC</p>
-              <p>/Feels like</p>
-              <p>12 ºC</p>
-            </div>
-          </div>
-        </li>
-        <li className="">
-          <div className="hour-info">
-            <p className="time">10</p>
-            <div className="icon">{CloudIcon()}</div>
-            <p className="description">Overcast Clouds</p>
-            <div className="temp">
-              <p>13 ºC</p>
-              <p>/Feels like</p>
-              <p>12 ºC</p>
-            </div>
-          </div>
-        </li>
-        <li className="">
-          <div className="hour-info">
-            <p className="time">13</p>
-            <div className="icon">{CloudIcon()}</div>
-            <p className="description">Overcast Clouds</p>
-            <div className="temp">
-              <p>13 ºC</p>
-              <p>/Feels like</p>
-              <p>12 ºC</p>
-            </div>
-          </div>
-        </li>
-        <li className="">
-          <div className="hour-info">
-            <p className="time">16</p>
-            <div className="icon">{CloudIcon()}</div>
-            <p className="description">Overcast Clouds</p>
-            <div className="temp">
-              <p>13 ºC</p>
-              <p>/Feels like</p>
-              <p>12 ºC</p>
-            </div>
-          </div>
-        </li>
-        <li className="">
-          <div className="hour-info">
-            <p className="time">18</p>
-            <div className="icon">{CloudIcon()}</div>
-            <p className="description">Overcast Clouds</p>
-            <div className="temp">
-              <p>13 ºC</p>
-              <p>/Feels like</p>
-              <p>12 ºC</p>
-            </div>
-          </div>
-        </li>
-        <li className="">
-          <div className="hour-info">
-            <p className="time">21</p>
-            <div className="icon">{CloudIcon()}</div>
-            <p className="description">Overcast Clouds</p>
-            <div className="temp">
-              <p>13 ºC</p>
-              <p>/Feels like</p>
-              <p>12 ºC</p>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </main>
+    <WeatherComponent apiOption={FORECAST_API_URL} Component={HoursTemplate} section="hours-section" />
   );
 };
 
--- src/pages/Today/Today.jsx
+++ src/pages/Today/Today.jsx
@@ -1,65 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Today.css';
-import { useCity } from '../../contexts/CityContext';
-import Spinner from '../../components/Spinner/Spinner';
-import Component from './Component';
-import { fetchWeatherData } from '../../services/weatherService';
-import { WEATHER_API_URL } from '../../services/API';
-
-const CACHE_EXPIRATION_TIME = 900000;
+import WeatherComponent from '../../components/WeatherComponent';
+import TodayTemplate from './TodayTemplate';
 
 const Today = () => {
-  const { selectedCity } = useCity();
-  const [weatherData, setWeatherData] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const loadData = async () => {
-      setLoading(true);
-      try {
-        const data = await fetchData(selectedCity, WEATHER_API_URL, 'weather');
-        setWeatherData(data);
-      } catch (error) {
-        console.error('Error fetching weather data:', error);
-      } finally {
-        setTimeout(() => setLoading(false), 500);
-      }
-    };
-
-    if (selectedCity) {
-      loadData();
-    }
-  }, [selectedCity]);
-
   return (
-    <main className="today-section">
-      {loading ? <Spinner /> : <Component selectedCity={selectedCity} weatherData={weatherData} />}
-    </main>
+    <WeatherComponent apiOption={'weather'} Component={TodayTemplate} section="today-section" />
   );
-};
-
-const fetchData = async (selectedCity, url, option) => {
-  const cachedData = getWeatherDataFromLocalStorage(selectedCity);
-  if (cachedData && !isDataExpired(cachedData)) {
-    return cachedData;
-  } else {
-    const data = await fetchWeatherData(selectedCity, url, option);
-    saveWeatherDataToLocalStorage(selectedCity, data);
-    return data;
-  }
-};
-
-const getWeatherDataFromLocalStorage = (selectedCity) => {
-  const cachedData = localStorage.getItem(selectedCity.label);
-  return cachedData ? JSON.parse(cachedData) : null;
-};
-
-const isDataExpired = (data) => {
-  return Date.now() - data.timestamp >= CACHE_EXPIRATION_TIME;
-};
-
-const saveWeatherDataToLocalStorage = (selectedCity, data) => {
-  localStorage.setItem(selectedCity.label, JSON.stringify(data));
 };
 
 export default Today;
