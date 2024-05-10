dfa9d02ecd2c34cf5c4d8ab67df4bc89e8dd1d4a
antaga04 <antaga04@gmail.com>
2024-05-10 20:21:27 +0200

    feat: create component for hour data from api

---
+++ src/components/HourData/HourData.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import './HourData.css';
+import {
+  ArrowCircleIcon,
+  CloudSolidIcon,
+  GaugeIcon,
+  HumidityIcon,
+  PrecipitationIcon,
+  VisibilityIcon,
+  WindIcon,
+} from '../Icons/Icons';
+import { transformTemperature, utcToLocal } from '../../utils/functions';
+import { useTemperatureUnit } from '../../contexts/TemperatureUnitContext';
+import { WeatherIcon } from '../Icons/WeatherIcon';
+
+const HourData = ({ data }) => {
+  const { unit } = useTemperatureUnit();
+
+  return (
+    <>
+      <div id="details">
+        <div className="hour-details">
+          <p className="time">{utcToLocal(data.dt)}</p>
+          <p className="description">{data.description}</p>
+        </div>
+        <div className="hour-info">
+          <p className="temp">{transformTemperature(data.temp, unit, true)}</p>
+          <WeatherIcon code={data.icon} css="mini-icon" />
+          <div className="pop">
+            <p>{Math.round(data.pop * 100)}%</p>
+            <PrecipitationIcon />
+          </div>
+        </div>
+      </div>
+      <div className={`hidden-details `}>
+        <div className="content">
+          <div>
+            <p>Humidity</p>
+            <p>{HumidityIcon()}</p>
+            <p>{data.humidity}%</p>
+          </div>
+          <div>
+            <p>Visibility</p>
+            <p>{VisibilityIcon()}</p>
+            <p>{data.visibility / 1000}km</p>
+          </div>
+          <div>
+            <p>Wind</p>
+            <p>{WindIcon()}</p>
+            <p>{Math.round(data.windSpeed * 3.6)}km/h</p>
+          </div>
+          <div>
+            <p>Degree</p>
+            <p style={{ transform: `rotate(${data.windDeg}deg)` }}>{ArrowCircleIcon()}</p>
+            <p>{data.windDeg}º</p>
+          </div>
+          <div>
+            <p>Clouds</p>
+            <p>{CloudSolidIcon()}</p>
+            <p>{data.clouds}%</p>
+          </div>
+          <div>
+            <p>Pressure</p>
+            <p>{GaugeIcon()}</p>
+            <p>{data.pressure}hPa</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HourData;
