a47c59455a1bd051daac34ae84f4fd40d1163730
antaga04 <antaga04@gmail.com>
2024-08-20 16:00:26 +0200

    fix: make loadOptions async

--- src/components/Searchbar/SearchBar.jsx
+++ src/components/Searchbar/SearchBar.jsx
@@ -6,23 +6,24 @@ import { GEO_API_URL, GEO_POPULATION, geoApiOptions } from '../../services/api';
 const SearchBar = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
 
-  const loadOptions = (inputValue) => {
-    return fetch(
-      `${GEO_API_URL}/cities?minPopulation=${GEO_POPULATION}&namePrefix=${inputValue}`,
-      geoApiOptions
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        return {
-          options: response?.data.map((city) => {
-            return {
-              value: `${city.latitude} ${city.longitude}`,
-              label: `${city.name}, ${city.countryCode}`,
-            };
-          }),
-        };
-      })
-      .catch((error) => console.error(error));
+  const loadOptions = async (inputValue) => {
+    try {
+      const response = await fetch(
+        `${GEO_API_URL}/cities?minPopulation=${GEO_POPULATION}&namePrefix=${inputValue}`,
+        geoApiOptions
+      );
+      const response_1 = await response.json();
+      return {
+        options: response_1?.data.map((city) => {
+          return {
+            value: `${city.latitude} ${city.longitude}`,
+            label: `${city.name}, ${city.countryCode}`,
+          };
+        }),
+      };
+    } catch (error) {
+      return console.error(error);
+    }
   };
 
   const handleOnChange = (searchData) => {
